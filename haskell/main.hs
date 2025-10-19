606226ecc7573d14dc668b41588167a9db190712
elliouros <elliouros@proton.me>
2025-10-19 11:46:11 -0400

    timer

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,8 +1,17 @@
-import System.Exit
+import System.CPUTime
 
-count :: Int -> IO ()
-count 1_000_000_000 = exitSuccess
+time :: Fractional b => a -> IO b
+time a = do
+  start <- getCPUTime
+  end <- seq a getCPUTime
+  return (fromIntegral (end - start) * 1e-12)
+
+count :: Int -> Int
+count 1_000_000_000 = 1_000_000_000
 count x =
   count (x + 1)
 
-main = count 1
+main =
+  time (count 1) >>= \x ->
+  putStr "Looping 1000000000 times in Haskell took: " >>
+  print x
